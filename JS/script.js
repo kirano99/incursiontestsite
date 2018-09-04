88d5e2adecc64400280e3d0eaa9ea78860af34fe
kirano99 <34285686+kirano99@users.noreply.github.com>
2018-09-04 11:25:35 +0100

    Beautify 1

--- JS/script.js
+++ JS/script.js
@@ -1,96 +1,103 @@
 window.onload = function () {
     init();
-   
+
 };
 
 function init() {
     console.log('init fired');
     //alert("ALERT");
 }
-    
-$(function(){       //OnLoad Function
+
+$(function () { //OnLoad Function
     $('#simple-menu').sidr();
     $(".navclass").fadeIn(5000).removeClass('hidden');
     $(".textlogo").fadeIn(5000).removeClass('hidden');
     console.log('animation');
     console.log('fired');
-    
+
     //for blog next/back pages VVVV
-    
-var $el = $("#wrap > div");
-var pageSize = 8;           //number of elements per page
 
-$el.slice(0, pageSize).css({background: 'black', display: 'block'});
-$el.slice(pageSize, $el.length).css({background: 'black', display: 'none'});
+    var $el = $("#wrap > div");
+    var pageSize = 8; //number of elements per page
 
-function addSlice(num){
-  return num + pageSize;
-}
+    $el.slice(0, pageSize).css({
+        background: 'black',
+        display: 'block'
+    });
+    $el.slice(pageSize, $el.length).css({
+        background: 'black',
+        display: 'none'
+    });
 
-function subtractSlice(num){
-  return num - pageSize;
-}
+    function addSlice(num) {
+        return num + pageSize;
+    }
 
-var slice = [0, pageSize];
+    function subtractSlice(num) {
+        return num - pageSize;
+    }
 
-$('.next').click(function(){
-   if (slice[1] < $el.length ){ 
-     slice = slice.map(addSlice);   
-   }
-   showSlice(slice);
-    
-    console.log(slice);
-    
-    $(".pagenum").html(slice[1] / pageSize);
-    
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-     return false;
-    
+    var slice = [0, pageSize];
+
+    $('.next').click(function () {
+        if (slice[1] < $el.length) {
+            slice = slice.map(addSlice);
+        }
+        showSlice(slice);
+
+        console.log(slice);
+
+        $(".pagenum").html(slice[1] / pageSize);
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+        return false;
+
+    });
+
+    $('.prev').click(function () {
+        if (slice[0] > 0) {
+            slice = slice.map(subtractSlice);
+        }
+        showSlice(slice);
+
+        console.log(slice);
+
+        $(".pagenum").html(slice[1] / pageSize);
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+        return false;
+
+    });
+
+    function showSlice(slice) {
+        $el.css('display', 'none');
+        $el.slice(slice[0], slice[1]).css('display', 'block');
+    }
 });
 
-$('.prev').click(function(){
-  if (slice[0] > 0 ){ 
-    slice = slice.map(subtractSlice); 
-  }
-  showSlice(slice);
-    
-    console.log(slice);
-    
-    $(".pagenum").html(slice[1] / pageSize);
-    
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-     return false;
-    
-});
-
-function showSlice(slice){
-  $el.css('display', 'none');
-  $el.slice(slice[0], slice[1]).css('display','block');
-}  
-});
 
 
+$(window).scroll(function () {
 
-  $(window).scroll(function() {
-
-    if ($(this).scrollTop()>400)
-     {
+    if ($(this).scrollTop() > 400) {
         $('.video-foreground').fadeOut();
-     }
-    else
-     {
-      $('.video-foreground').fadeIn();
-     }
- });
+    } else {
+        $('.video-foreground').fadeIn();
+    }
+});
 
-$(function(){
+$(function () {
     $(".flip").flip({
         trigger: 'click'
     });
 });
 
 
-	function openNav() {
+function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     console.log("worked");
 }
@@ -98,6 +105,3 @@ $(function(){
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 }
-
-
-
